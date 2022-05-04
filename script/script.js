4663bc1ba2f252490124a8e82ef29be04a8a4beb
Ananth Krishnan A <ab.ananth97@gmail.com>
2022-05-04 23:49:05 +0530

    added login

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
-var user_name = document.getElementById("user_name");
-var user_password = document.getElementById("user_password");
+var uname = document.getElementById("user_name");
+var upassword = document.getElementById("user_password");
 
-console.log(user_name);
-// console.log(user_password);
+function signup(){
+    if(uname.value == "" || upassword.value ==""){
+        alert("in if");
+        // me_user.innerText="required";
+        // me_user.style.color="red";
+        me_password.innerText="required";
+        me_password.style.color="red";
+        var flag =  false;
+    }else{
+        alert("else");
+        var flag = true;
+    }
+    return flag;
+}
