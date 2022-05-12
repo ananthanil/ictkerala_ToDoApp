e3e5967b905dc01998cacccccec65c43eb739ca9
Ananth Krishnan A <ab.ananth97@gmail.com>
2022-05-12 23:13:07 +0530

    added script content

--- script/script.js
+++ script/script.js
@@ -1,14 +1,18 @@
 var loginemail = document.getElementById("loginemail");
 var loginpwd = document.getElementById("loginpwd");
-function signin(){
-    if(loginemail=="admin" || loginpwd==12345){
-        return true;
-    }else{
+
+function signin(callback){
+    alert("in function");
+    if(loginemail.value == "admin" && loginpwd.value == 12345){
+        alert("in if");
+        callback();
+        }else{
+        alert("in else");
         error_message.innerText = "username or password is incorrect"
         return false;
     }
 }
 
-function logout(){
-    window.location.href = "../index.html";
+function redirect(){
+    window.location.href = "../login_welcome_page.html";
 }
