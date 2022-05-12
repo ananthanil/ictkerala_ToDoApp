91c8e78f3baa2bf535bcaeb10d6d2381dee3db55
Ananth Krishnan A <ab.ananth97@gmail.com>
2022-05-13 00:26:45 +0530

    added login properly

--- script/script.js
+++ script/script.js
@@ -1,18 +1,16 @@
-var loginemail = document.getElementById("loginemail");
-var loginpwd = document.getElementById("loginpwd");
-
 function signin(callback){
-    alert("in function");
-    if(loginemail.value == "admin" && loginpwd.value == 12345){
-        alert("in if");
+    let loginemail = document.getElementById("loginemail");
+    let loginpwd = document.getElementById("loginpwd");
+    if(loginemail.value.trim()=="admin" && loginpwd.value.trim()=="12345"){
         callback();
         }else{
-        alert("in else");
-        error_message.innerText = "username or password is incorrect"
+        alert("username or password is incorrect");
         return false;
     }
 }
 
 function redirect(){
-    window.location.href = "../login_welcome_page.html";
+    
+   window.location.href="login_welcome_page.html"
+    alert("ad ello");
 }
