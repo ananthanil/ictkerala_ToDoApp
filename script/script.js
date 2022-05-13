33fe4f533e7e99a78dbc04d0be6845baf19daf0d
Ananth Krishnan A <ab.ananth97@gmail.com>
2022-05-13 12:23:23 +0530

    added pop

--- script/script.js
+++ script/script.js
@@ -42,4 +42,48 @@ function displaylist(){
         alert(e.description);
     }
 }
- 
+
+function display(data){
+    var list = JSON.parse(data);
+    let table = document.getElementById("todotable");
+
+    for(var i=0;i<list.length;i++){
+        let rowcount = table.rows.length;
+        var row = table.insertRow(rowcount);
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML=list[i].id;
+
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML=list[i].title;
+
+        var cell3 = row.insertCell(2);
+        var element = document.createElement("input");
+        element.type="checkbox";
+
+        if(list[i].completed==true){
+            element.setAttribute("checked","true");
+            element.setAttribute("disabled","true");
+        }
+        element.addEventListener('change',(event)=>{
+            if(event.currentTarget.checked){
+                count++;
+                checkCounter();
+            }else{
+                count--;
+            }
+        })
+        cell3.appendChild(element);
+    }
+}
+
+var count = 0;
+function  checkCounter(){
+    let promise = new Promise(function(resolve,reject){
+        if (count==5) {
+            resolve("congrats ! 5 Tasks Completed");
+        }
+    })
+    promise.then(function(s){
+        alert(s);
+    })
+}
