771cd7241e99d82fc8f222018fb21fc8e230bc48
Ananth Krishnan A <ab.ananth97@gmail.com>
2022-05-13 11:55:21 +0530

    added todolist link

--- script/script.js
+++ script/script.js
@@ -17,7 +17,29 @@ function signout(){
     window.location.href="../index.html"
  }
 
-function display(){
-    
+function displaylist(){
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange=function(){
+        try{
+            if(httpRequest.readyState===XMLHttpRequest.DONE){
+                if(httpRequest.status===200){
+                    display(httpRequest.responseText)
+                }
+                else{
+                    alert("error from API");
+                }
+            }
+        }
+        catch(e){
+            alert(e.description);
+        }
+    };
+    try{
+        httpRequest.open('Get','https://jsonplaceholder.typicode.com/todos',true);
+        httpRequest.send();
+    }
+    catch(e){
+        alert(e.description);
+    }
 }
  
